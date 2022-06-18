77190ad6a1fab580b1d7343420b68a555391c1dc
angelinalos <newyorksquirrel33@gmail.com>
2022-06-18 15:27:37 +0300

    hw_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Button } from "./components/Button/Button";
+import { EmojiContainer } from './components/EmojiContainer/EmojiContainer';
 import {User} from "./components/User/User"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col-reverse items-center">
       <div className='UserButton'>
        <User text="Shawn Connory"/>
        <Button disabled={true} text = "" type={"primary"}/>
       </div>
+      <EmojiContainer />
     </div>
   );
 }
